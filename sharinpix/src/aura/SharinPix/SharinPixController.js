e079f995291dedcdba252025dd12639d32bb9d30
Luc Boissaye <luc@boissaye.fr>
2016-06-17 12:51:22 +0200

    Fix lightning bugs

--- sharinpix/src/aura/SharinPix/SharinPixController.js
+++ sharinpix/src/aura/SharinPix/SharinPixController.js
@@ -1,41 +1,42 @@
 ({
-  doInit : function(component, event, helper) {
+  doInit : function(component) {
     var action = component.get('c.getToken');
-    action.setParams({ albumId : component.get('v.AlbumId') });
+    action.setParams({ albumId : component.get('v.AlbumId') || component.get('v.recordId') });
     action.setCallback(this, function(response) {
-      if (response.state == 'SUCCESS') {
-        component.set('v.url', response.returnValue);
+      var state = response.getState();
+      if (state === 'SUCCESS') {
+        component.set('v.url', response.getReturnValue());
       } else {
-        component.set('v.error', response.error[0].message);
         component.set('v.loading', false);
+        component.set('v.error', 'SharinPix Token Error');
       }
     });
     $A.enqueueAction(action);
 
-    var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+    /*var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
     var eventer = window[eventMethod];
-    var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-    eventer(messageEvent,function(e) {
+    var messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
+    eventer(messageEvent, function(e) {
       if (component.find('iframe') && component.find('iframe').getElement().contentWindow === e.source) {
-        var event = $A.get("e.c:Event");
+        /*var event = $A.get("e.c:Event");
         event.setParams({
           "name" : e.data.name,
           "payload": e.data.payload,
           "albumId": component.get('v.AlbumId'),
           "source": component
          });
-        event.fire();
+        event.fire();* /
 
-        if (e.data.name == 'viewer-image-viewed') {
+        if (e.data.name === 'viewer-image-viewed') {
           component.set('v.fullscreen', true);
         }
-        if (e.data.name == 'viewer-closed') {
+        if (e.data.name === 'viewer-closed') {
           component.set('v.fullscreen', false);
         }
       }
-    },false);
+    }, false);*/
   },
-  onLoaded : function(component, event) {
+  onLoaded : function(component) {
     component.set('v.loading', false);
   }
 })
