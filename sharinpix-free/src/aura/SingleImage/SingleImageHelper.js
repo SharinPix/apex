29469c114d5237b8d4c883eff5476678d81abb3b
Kevan <kevan.moothien@spoonconsulting.com>
2017-01-11 16:28:24 +0400

    retrieve contentdocument and display

--- sharinpix-free/src/aura/SingleImage/SingleImageHelper.js
+++ sharinpix-free/src/aura/SingleImage/SingleImageHelper.js
@@ -1,3 +1,16 @@
 ({
-
+	reloadSource: function(component) {
+		var source = component.get('v.attachmentId');
+		if (!$A.util.isEmpty(source) && (source.length === 15 || source.length === 18)) {
+			if (source.startsWith('00P')){
+				component.set('v.source', '/servlet/servlet.FileDownload?file=' + source);	
+			}
+			else if (source.startsWith('068')){
+				component.set('v.source', '/sfc/servlet.shepherd/version/download/'+source);
+			}
+		}
+		else {
+			component.set('v.source', source);
+		}
+	}
 })
