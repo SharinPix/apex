29e66dd8e0a69f00e5efc4a0184dc6926d41f4e8
Kevan <kevan.moothien@spoonconsulting.com>
2017-01-30 10:51:53 +0400

    WIP

--- sharinpix-free/src/aura/Uploader/UploaderController.js
+++ sharinpix-free/src/aura/Uploader/UploaderController.js
@@ -6,6 +6,32 @@
 		component.set('v.progress', 0);
 		component.set('v.done', false);
 		component.set('v.uploaderId', 'uploader_'+component.getGlobalId());
+
+
+
+
+	    if (component.isValid()){
+	      var baseUrl = window.location.protocol + '//' + window.location.hostname;
+	      console.log(component);
+	      component.set('v.iframeUrl', '/apex/test_upload?url='+baseUrl);
+	    }
+
+	    window.addEventListener('message', $A.getCallback( function(postMessageEvent) {
+	        // if (postMessageEvent.origin !== 'https://app.sharinpix.com'){
+	        //   return;
+	        // }
+	        if (postMessageEvent && component.isValid() ){
+	          console.log('Unhandled event:', postMessageEvent.data);
+	        }
+	      })
+	    );
+
+	    // setInterval(function(event){
+	    //   component.find('upload-iframe').getElement().contentWindow.postMessage('loaded parent', '*');
+	    // }, 3000)
+
+
+
 	},
 	fileInputChange: function(component, event, helper) {
 		helper.upload(component, component.find("file").getElement().files, function(err, res){
@@ -15,5 +41,7 @@
         		eventUploaded.fire();
 			}
 		});	
+
+		component.find('upload-iframe').getElement().contentWindow.postMessage({files: component.find('file').getElement().files}, '*');
 	}
 })
