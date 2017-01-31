1b9295cced7daa6c94dad7b0f71d0734a2ec46f7
Kevan <kevan.moothien@spoonconsulting.com>
2017-01-31 18:08:58 +0400

    WIP attachment upload

--- sharinpix-free/src/aura/Uploader/UploaderController.js
+++ sharinpix-free/src/aura/Uploader/UploaderController.js
@@ -7,9 +7,6 @@
 		component.set('v.done', false);
 		component.set('v.uploaderId', 'uploader_'+component.getGlobalId());
 
-
-
-
 	    if (component.isValid()){
 	      var baseUrl = window.location.protocol + '//' + window.location.hostname;
 	      console.log(component);
@@ -21,7 +18,25 @@
 	        //   return;
 	        // }
 	        if (postMessageEvent && component.isValid() ){
-	          console.log('Unhandled event:', postMessageEvent.data);
+				if (postMessageEvent.data.eventIdentifier == component.getGlobalId()){
+					console.log('Unhandled event:', postMessageEvent.data);
+					if (postMessageEvent.data.name=='uploading'){
+						component.set('v.progress', postMessageEvent.data.percent);
+					}
+					if (postMessageEvent.data.name=='uploaded'){
+						component.set('v.n_uploaded', 0);
+						component.set('v.n_uploading', 0);
+						component.set('v.progress', 0);
+						component.set('v.done', true);
+						var input = component.find("file").getElement();
+						input.type = '';
+						input.type = 'file';
+
+						var eventUploaded = $A.get('e.c:Uploaded')
+						eventUploaded.setParam('eventIdentifier', component.get('v.eventIdentifier'));
+						eventUploaded.fire();
+					}		
+				}
 	        }
 	      })
 	    );
@@ -34,14 +49,18 @@
 
 	},
 	fileInputChange: function(component, event, helper) {
-		helper.upload(component, component.find("file").getElement().files, function(err, res){
-			if (err !== 'Error occurred'){
-				var eventUploaded = $A.get('e.c:Uploaded')
-				eventUploaded.setParam('eventIdentifier', component.get('v.eventIdentifier'));
-        		eventUploaded.fire();
-			}
-		});	
+		// helper.upload(component, component.find("file").getElement().files, function(err, res){
+		// 	if (err !== 'Error occurred'){
+		// 		var eventUploaded = $A.get('e.c:Uploaded')
+		// 		eventUploaded.setParam('eventIdentifier', component.get('v.eventIdentifier'));
+  //       		eventUploaded.fire();
+		// 	}
+		// });	
 
-		component.find('upload-iframe').getElement().contentWindow.postMessage({files: component.find('file').getElement().files}, '*');
+		component.find('upload-iframe').getElement().contentWindow.postMessage({name: 'new-upload', 
+																				recordId: component.get('v.recordId'),
+																				eventIdentifier: component.getGlobalId(), 
+																				files: component.find('file').getElement().files
+																				}, '*');
 	}
 })
