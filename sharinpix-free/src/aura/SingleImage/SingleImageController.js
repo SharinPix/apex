f45d7c32fb32c97d2a5ed3f036d39665731e424c
Kevan <kevan.moothien@spoonconsulting.com>
2017-02-14 15:12:22 +0400

    site url for community

--- sharinpix-free/src/aura/SingleImage/SingleImageController.js
+++ sharinpix-free/src/aura/SingleImage/SingleImageController.js
@@ -1,16 +1,17 @@
 ({
 	doInit : function(component, event, helper) {
+		helper.site_url(component);
 		helper.reloadSource(component);
 
 		var height = component.get('v.height');
 		if (typeof height !== "undefined" && height !== null && height > 200) {
-			
+
 		}else{
 			height = 200;
 		}
 		component.set('v.style', 'height: '+height+'px; line-height: '+height+'px;');
 	},
-	doReload: function(component, event, helper){		
+	doReload: function(component, event, helper){
 		component.set('v.loading', true);
 		helper.reloadSource(component)
 	},
